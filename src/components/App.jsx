d395d97565bd8164a39b79a69e68765da8b7cfe7
DenysVM <muratov.d@gmail.com>
2023-09-03 19:45:23 +0200

    1. Styles of FriendList in 1 file 2. Data move to src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from "./Profile/Profile";
-import user from './Data/user.json'; 
+import user from '../Data/user.json'; 
 import { Statistics } from "./Statistics/Statistics";
-import data from './Data/data.json';
+import data from '../Data/data.json';
 import { FriendList } from "./FriendList/FriendList"
-import friends from './Data/friends.json'
+import friends from '../Data/friends.json'
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory"
-import transactions from './Data/transactions.json'
+import transactions from '../Data/transactions.json'
 
 export const App = () => {
   return (
