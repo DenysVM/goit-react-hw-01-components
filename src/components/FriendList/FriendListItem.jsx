a35368e6d5de40cc51ea2119aa64ecfe9b958d83
DenysVM <muratov.d@gmail.com>
2023-09-03 10:57:45 +0200

    1. Файли з даними зібрані в одну папку Data. 2. Перероблений розділ FriendList - відпрацювання двома компонентами.

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import styles from './FriendListItem.module.css';
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.friend_list_item}>
+      <span className={styles.status} style={{ backgroundColor: isOnline ? 'green' : 'red', }}>{isOnline}</span>
+      <img className={styles.avatar} src={avatar} alt="User avatar" width="48" height="48" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
